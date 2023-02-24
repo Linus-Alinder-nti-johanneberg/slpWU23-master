1b28e48046b3330e41edcada0263848419936c68
Linus-Alinder-nti-johanneberg <linus.alinder@elev.ga.ntig.se>
2023-02-24 14:31:46 +0100

    navmeny + börja på animation

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+const checkbox = document.getElementById('burger');
+const div = document.getElementById('menu_items');
+menu_items.style.display = 'none';
+
+checkbox.addEventListener('click', function handleClick() {
+    if (checkbox.checked) {
+        menu_items.style.display = 'block';
+    } else {
+        menu_items.style.display = 'none';
+    }
+  });
