865f064b9625d0bc8d7b6bd340cac095453cd6d7
Linus-Alinder-nti-johanneberg <linus.alinder@elev.ga.ntig.se>
2023-03-06 16:01:09 +0100

    fixat cards
    gjort en template
    fixat javascript som saka kunna visa mer information på kortet

--- js/script.js
+++ js/script.js
@@ -9,5 +9,17 @@ checkbox.addEventListener('click', function handleClick() {
     } else {
         menu_items.style.display = 'none';
     }
-  });
+});
   
+const div1 = document.getElementById('more_text');
+var button = document.getElementById('show_more');
+button.addEventListener('click', toggle);
+more_text.style.display = 'none';
+
+function toggle(){
+    if (more_text.style.display == 'none') {
+        more_text.style.display = 'block';
+      } else {
+        more_text.style.display = 'none';
+      }
+};
